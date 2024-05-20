b084a2af7aee9b0415988441e27defbb0ea7b8f4
Adarsh Shaju <123304718+adarshshajub@users.noreply.github.com>
2024-05-20 20:33:27 +0530

    update build file

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Building project packages----"
-python3 -m pip install -r ./requirements.txt
+python3.9 -m pip -r requirements.txt
 
-echo "Migrating Database...."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# echo "Migrating Database...."
+# python3.9 manage.py makemigrations 
+# python3.9 manage.py migrate 
 
 echo "Collecting static files...."
-python3 manage.py collectstatic --noinput
-
-# echo "runserver"
-# python3 manage.py runserver
+python3.9 manage.py collectstatic 
 
 
 
