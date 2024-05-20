deb802bfd26cb452e4c3acf77d76b82d94356b88
Adarsh Shaju <123304718+adarshshajub@users.noreply.github.com>
2024-05-20 20:36:59 +0530

    build file update

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Building project packages----"
-python -m pip -r requirements.txt
+python3 -m pip -r requirements.txt
 
 # echo "Migrating Database...."
 # python3.9 manage.py makemigrations 
 # python3.9 manage.py migrate 
 
 echo "Collecting static files...."
-python manage.py collectstatic 
+python3 manage.py collectstatic 
 
 
 
