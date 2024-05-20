020c711ba0038a61a5546411e648fd57f2fb74b1
Adarsh Shaju <123304718+adarshshajub@users.noreply.github.com>
2024-05-20 19:37:17 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,14 +2,14 @@
 
 echo "Building project packages----"
 
-python -m pip -r requirements.txt
+python3 -m pip -r ./requirements.txt
 
 echo "Migrating Database...."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collecting static files...."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 
 
