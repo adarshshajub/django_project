b9b3fd0ae4ba6443b098a2bec35d0f2c6d008dfb
Adarsh Shaju <123304718+adarshshajub@users.noreply.github.com>
2024-05-20 19:39:52 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -5,11 +5,11 @@ pip install -r requirements. txt
 # python3 -m pip -r ./requirements.txt
 
 echo "Migrating Database...."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collecting static files...."
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 
 
